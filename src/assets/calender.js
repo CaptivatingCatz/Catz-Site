a97c72cdd8fcbdcb986823906b82ac9655bcc4e3
Chrisl11-llc <Depolarizedhope@gmail.com>
2023-10-27 16:17:17 -0500

    getDateFunctionality

--- src/assets/calender.js
+++ src/assets/calender.js
@@ -1,8 +1,15 @@
 const date = new Date();
 console.log(date)
 let month = date.getMonth();
+let day = date.getDay();
+let currentDay = date.getDate();
+let currentYear = date.getFullYear();
+
+//<!--add one to account for zero as 1 -->
+//console.log(month + 1);
 let monthWord = "";
-// get the corrent month into a string to send back to the calender
+let dayWord = "";
+//<!-- get the corrent month into a string to send back to the calender -->
 switch (date.getMonth()) {
   case 0:
     monthWord = "January";
@@ -40,8 +47,41 @@ switch (date.getMonth()) {
     monthWord = "December";
     break;
 }
-// Varible monthWord contains the current Month
-console.log("MonthWord " + monthWord);
+
+switch (date.getDay()) {
+  case 0:
+    dayWord = "Sunday";
+    break;
+  case 1:
+    dayWord = "Monday";
+    break;
+  case 2:
+     dayWord = "Tuesday";
+    break;
+  case 3:
+   dayWord = "Wednesday";
+    break;
+  case 4:
+    dayWord = "Thursday";
+    break;
+  case 5:
+    dayWord = "Friday";
+    break;
+  case 6:
+    dayWord = "Saturday";
+    break;
+}
+//console outputs
+console.log("DayWord: " + dayWord);
+console.log("MonthWord: " + monthWord);
+console.log("CurrentDay: " + currentDay);
+console.log("CurrentYear: " + currentYear);
+//console.log("Day: " + day);
+
+
 //document.dom.title() = monthword;
-let displayElement = document.querySelector("#title");
-displayElement.innerHTML = monthWord;
+let monthElement = document.querySelector("#title");
+monthElement.innerHTML = monthWord;
+
+let dayElement = document.querySelector("#weekDay");
+dayElement.innerHTML = dayWord;
